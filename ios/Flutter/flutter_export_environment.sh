aaa5d7efb94d75bdf692bca9397083f93441e0b1
sagarfaldu17 <sagar.faldu@mindinventory.com>
2022-06-04 00:41:08 +0530

    feat(): update pod incase of direct run

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mac-00016/Documents/IDE Flutter/Stable release Flutter SDK/flutter2.5/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Projects/FLUTTERPROJECTS/DigitalDemo"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Projects/FLUTTERPROJECTS/digital14_demo"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Volumes/Projects/FLUTTERPROJECTS/DigitalDemo/lib/main.dart"
+export "FLUTTER_TARGET=/Volumes/Projects/FLUTTERPROJECTS/digital14_demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Volumes/Projects/FLUTTERPROJECTS/DigitalDemo/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Volumes/Projects/FLUTTERPROJECTS/digital14_demo/.dart_tool/package_config.json"
